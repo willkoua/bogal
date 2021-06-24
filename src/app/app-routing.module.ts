c05df313f55e971b7c2240c9d507274d5e0287be
willy kouagnia <willkoua@gmail.com>
2021-06-24 08:10:43 -0400

    add homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
 
 const routes: Routes = [
-  {
+  /*{
     path: '',
-    // component: MainLayoutComponent,
     loadChildren: () =>
       import('./main/closed/closed.module').then(
         m => m.ClosedModule
+      ),
+  },*/
+  {
+    path: '',
+    component: MainLayoutComponent,
+    loadChildren: () =>
+      import('./main/static/static.module').then(
+        m => m.StaticModule
       ),
   },
 ];
