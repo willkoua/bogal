4cc34932c2e3c0aacf2b7aaf95aa9d4952ae7b85
willy kouagnia <willkoua@gmail.com>
2021-07-06 15:24:02 -0400

    add angular animation

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,12 +1,26 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {
+  bounceInOnEnterAnimation,
+  bounceOutOnLeaveAnimation,
+  fadeInOnEnterAnimation,
+  fadeOutOnLeaveAnimation
+} from 'angular-animations';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
+  animations: [
+    fadeInOnEnterAnimation({ anchor: 'enter', duration: 1000, delay: 100, }),
+    fadeOutOnLeaveAnimation({ anchor: 'leave', duration: 1000, delay: 100, }),
+    bounceInOnEnterAnimation({ anchor: 'bounceEnter', duration: 1000, delay: 100 }),
+    bounceOutOnLeaveAnimation({ anchor: 'bounceLeave', duration: 1000, delay: 100 }),
+  ]
 })
 export class MainLayoutComponent implements OnInit {
   showMenu = false;
+  enableLink = false;
+  enableMenu = false;
 
   constructor() { }
 
@@ -15,6 +29,25 @@ export class MainLayoutComponent implements OnInit {
 
   toggleMenu(): void {
     this.showMenu = !this.showMenu;
-    console.log(this.showMenu);
+    if (this.showMenu) {
+      this.enableMenu = true;
+    } else {
+      this.enableLink = false;
+      this.enableMenu = false;
+    }
+  }
+
+  enterMenuDone(event: AnimationEvent): void {
+    // @ts-ignore
+    if (event.toState !== 'void') {
+      this.enableLink = true;
+    }
+  }
+
+  leaveMenuDone(event: AnimationEvent): void {
+    // @ts-ignore
+    if (event.toState === 'void') {
+      this.enableLink = false;
+    }
   }
 }
--- src/app/shared/main-header/main-header.component.ts
+++ src/app/shared/main-header/main-header.component.ts
@@ -1,12 +1,19 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
-  styleUrls: ['./main-header.component.scss']
+  styleUrls: ['./main-header.component.scss'],
+  animations: [
+    fadeInOnEnterAnimation({ anchor: 'enter', duration: 1000, delay: 100, }),
+    fadeOutOnLeaveAnimation({ anchor: 'leave', duration: 1000, delay: 100, })
+  ]
 })
 export class MainHeaderComponent implements OnInit {
   @Output() activeMenu: EventEmitter<boolean> = new EventEmitter<boolean>();
+  closeBtn = false;
+  showBtn = true;
   showMenu = false;
 
   constructor() { }
@@ -16,6 +23,40 @@ export class MainHeaderComponent implements OnInit {
 
   toggleMenu(): void {
     this.showMenu = !this.showMenu;
+    if (this.showMenu) {
+      this.showBtn = false;
+    } else {
+      this.closeBtn = false;
+    }
     this.activeMenu.emit(this.showMenu);
   }
+
+  enterShowDone(event: AnimationEvent): void {
+    // @ts-ignore
+    if (event.toState !== 'void') {
+      this.showBtn = true;
+    }
+  }
+
+  leaveShowDone(event: AnimationEvent): void {
+    // @ts-ignore
+    if (event.toState === 'void') {
+      this.closeBtn = true;
+    }
+  }
+
+  enterCloseDone(event: AnimationEvent): void {
+    // @ts-ignore
+    if (event.toState !== 'void') {
+      this.closeBtn = true;
+    }
+  }
+
+  leaveCloseDone(event: AnimationEvent): void {
+    // @ts-ignore
+    if (event.toState === 'void') {
+      this.showBtn = true;
+    }
+  }
+
 }
