2228517caf0ae05da4fdc5a233f7e0efabb581a7
willy kouagnia <willkoua@gmail.com>
2021-07-06 08:57:51 -0400

    add main header component

--- src/app/shared/main-header/main-header.component.ts
+++ src/app/shared/main-header/main-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-main-header',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-header.component.scss']
 })
 export class MainHeaderComponent implements OnInit {
+  @Output() activeMenu: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() showMenu = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleMenu(): void {
+    this.activeMenu.emit(!this.showMenu);
+  }
 }
