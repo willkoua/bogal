fcf65818df551ed582b081f85a29accd519e4a49
willy kouagnia <willkoua@gmail.com>
2022-10-28 00:53:17 -0400

    upgrade version angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +10,15 @@ import {MatSliderModule} from '@angular/material/slider';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {ToastrModule} from 'ngx-toastr';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateCompiler, TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {MESSAGE_FORMAT_CONFIG, TranslateMessageFormatCompiler} from 'ngx-translate-messageformat-compiler';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {Const} from '../environments/const';
+
+function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -27,11 +35,33 @@ import {HttpClientModule} from '@angular/common/http';
       timeOut: 7000,
       progressBar: true
     }),
+    TranslateModule.forRoot({
+      defaultLanguage: Const.app.lang.fr,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: TranslateMessageFormatCompiler
+      }
+    }),
     MatSliderModule,
     MatSidenavModule,
     MatToolbarModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: Const.app.lang.fr},
+    {
+      provide: MESSAGE_FORMAT_CONFIG, useValue: {
+        locales: [
+          Const.app.lang.fr,
+          Const.app.lang.en
+        ]
+      }
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
